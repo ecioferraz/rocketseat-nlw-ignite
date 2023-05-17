fd601282d0f95247ffd67b1fc4d221aa4d94913a
Écio Ferraz <ecioferraz@gmail.com>
2023-05-17 00:09:40 -0300

    chore: cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, { origin: true })
+// todas as URLs de front-end poderão acessar nosso back-end
+// origin: ['site1', 'site2'...] para especificar permissões
 app.register(memoriesRoutes)
 
 app
