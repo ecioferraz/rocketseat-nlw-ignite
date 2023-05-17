ed705ec91c5fb053e03c70dc0ce8f46aef971bdf
Écio Ferraz <ecioferraz@gmail.com>
2023-05-16 22:58:13 -0300

    chore: svg transformer

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
+import { ImageBackground } from 'react-native'
+import { styled } from 'nativewind'
 
 import {
   Roboto_400Regular as Roboto400Regular,
@@ -7,6 +8,11 @@ import {
   useFonts,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_700Bold as BaiJamjuree700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -18,11 +24,13 @@ export default function App() {
   if (!hasLoadedFonts) return null
 
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="font-title text-5xl font-bold text-gray-50">
-        Hello, world!
-      </Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+    >
+      <StyledStripes className="absolute left-0" />
+
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
