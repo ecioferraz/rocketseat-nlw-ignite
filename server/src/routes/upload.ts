b2194e14b9c92ca5e58d5149b52ed7d0de40d138
Écio Ferraz <ecioferraz@gmail.com>
2023-05-19 13:06:53 -0300

    feat: file upload

---
+++ server/src/routes/upload.ts
@@ -0,0 +1,40 @@
+import { randomUUID } from 'crypto'
+import { FastifyInstance } from 'fastify'
+import { createWriteStream } from 'fs'
+import { resolve, extname } from 'path'
+import { pipeline } from 'stream'
+import { promisify } from 'util'
+
+const pump = promisify(pipeline)
+
+export default async function uploadRoutes(app: FastifyInstance) {
+  app.post('/upload', async (req, res) => {
+    console.log('upload')
+    const upload = await req.file({
+      limits: { fileSize: 5_242_880 /* 5mb */ },
+    })
+
+    if (!upload) return res.status(400).send()
+
+    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
+    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
+
+    if (!isValidFileFormat) return res.status(400).send()
+
+    const fileId = randomUUID()
+    const extension = extname(upload.filename)
+
+    const fileName = fileId.concat(extension)
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads/', fileName),
+    )
+
+    await pump(upload.file, writeStream)
+
+    const fullUrl = req.protocol.concat('://').concat(req.hostname)
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+
+    return { fileUrl }
+  })
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,22 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
+import fastifyStatic from '@fastify/static'
+
 import { memoriesRoutes } from './routes/memories'
 import authRoutes from './routes/auth'
+import uploadRoutes from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(fastifyStatic, {
+  prefix: '/upload',
+  root: resolve(__dirname, '../uploads'),
+})
 
 app.register(cors, { origin: true })
 // todas as URLs de front-end poderão acessar nosso back-end
@@ -15,6 +27,7 @@ app.register(cors, { origin: true })
 app.register(jwt, { secret: 'spacetime' })
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({ host: '0.0.0.0', port: 3333 })
